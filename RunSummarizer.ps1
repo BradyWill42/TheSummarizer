b51a20ea86eb6a02cc2c900cb142b85bb80dfda7
BradyWill42 <bradymonster8@gmail.com>
2025-01-12 19:13:01 -0500

    Added support for API KEY

--- RunSummarizer.ps1
+++ RunSummarizer.ps1
@@ -7,4 +7,10 @@ Unblock-File -Path .\.venv\Scripts\Activate.ps1
 
 .\.venv\Scripts\activate.ps1
 
+Unblock-File -Path .\API_KEY.ps1
+
+if (Test-Path .\API_KEY.ps1) {
+    .\API_KEY.ps1
+}
+
 py SummarizeTaxTable.py
