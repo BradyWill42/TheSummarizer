7e9d124dbcd9bec599f6bd8156b51f1eb4e8b55f
BradyWill42 <bradymonster8@gmail.com>
2025-01-12 19:19:36 -0500

    Finalizing Scripts to ensure .ps1 API+KEY file works

--- RunSummarizer.ps1
+++ RunSummarizer.ps1
@@ -3,14 +3,19 @@ $DIRECTORY = (Get-Item .).FullName
 Write-Output $DIRECTORY
 Set-Location $DIRECTORY
 
-Unblock-File -Path .\.venv\Scripts\Activate.ps1
-
-.\.venv\Scripts\activate.ps1
-
-Unblock-File -Path .\API_KEY.ps1
+if (Test-Path .\.venv\Scripts\Activate.ps1) {
+	Unblock-File -Path .\.venv\Scripts\Activate.ps1
+	.\.venv\Scripts\activate.ps1
+} else {
+	Write-Output "Virtual Environment Script does not exist or failed"
+}
 
 if (Test-Path .\API_KEY.ps1) {
-    .\API_KEY.ps1
+	Unblock-File -Path .\API_KEY.ps1
+	.\API_KEY.ps1
+} else {
+	Write-Output "API_KEY.ps1 Missing"
+	pause
 }
 
 py SummarizeTaxTable.py
